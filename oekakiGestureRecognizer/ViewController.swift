222fc137b64f9c7f8b278fbf94eb3462a01e47c0
anthrgrnwrld <anthrgrnwrld@gmail.com>
2016-06-28 17:49:38 +0900

    Initial Commit

---
+++ oekakiGestureRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oekakiGestureRecognizer
+//
+//  Created by Masaki Horimoto on 2016/06/28.
+//  Copyright © 2016年 Masaki Horimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
