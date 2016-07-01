c5e7c783e03fe5c5a761718b8d8b5a5e08faa287
anthrgrnwrld <anthrgrnwrld@gmail.com>
2016-07-01 19:16:12 +0900

    add how to use (example)

--- oekakiGestureRecognizer/ViewController.swift
+++ oekakiGestureRecognizer/ViewController.swift
@@ -10,14 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let myOekaki = OekakiGestureRecognizer(target: self, action: #selector(ViewController.drawGesture(_:)))
+        self.view.addGestureRecognizer(myOekaki)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    internal func drawGesture(sender: AnyObject) {
+        
+        guard let oekakiGesture = sender as? OekakiGestureRecognizer else {
+            print("Oekaki Gesture Error happened.")
+            return
+        }
+        
+        switch oekakiGesture.state {
+            
+        case .Began:
+            
+            let size = self.imageView.frame.size                                //コンテキストのサイズ
+            let touchPoint = oekakiGesture.locationInView(self.imageView)       //タッチ座標を取得
+            
+            //Drawを実行 -> 結果をUIImageにて取得
+            let imageAfterDraw = drawCircle(self.imageView.image, size: size, touchPoint: touchPoint)
+            self.imageView.image = imageAfterDraw
+            
+        default:
+            break
+            
+        }
+    
+    }
+    
+    func drawCircle(image: UIImage?, size: CGSize, touchPoint: CGPoint) -> UIImage {
+        
+        let radius: CGFloat = 20.0                                          //Drawする円の半径
+        let red: CGFloat = 1.0                                              //Drawする色 R
+        let blue: CGFloat = 0.0                                             //Drawする色 B
+        let green: CGFloat = 0.0                                            //Drawする色 G
+        let alpha: CGFloat = 1.0                                            //Drawする色 Alpha
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)            //コンテキストを取得
+        
+        //タップした時に描く円のRect
+        let circleRect = CGRectMake(touchPoint.x - radius, touchPoint.y - radius, radius * 2, radius * 2)
+        
+
+        image?.drawInRect(CGRectMake(0, 0, size.width, size.height))        //コンテキストにimageViewの内容を写す
+        let circlePath = UIBezierPath(ovalInRect: circleRect)               //円を描く
+        let color = UIColor(red:red, green:green, blue:blue, alpha:alpha)   //透明色を格納
+        color.setFill()
+        circlePath.fill()
+        let retImage = UIGraphicsGetImageFromCurrentImageContext()          //UIImageを取得する
+        UIGraphicsEndImageContext()                                         //コンテキストを閉じる
+        
+        return retImage                                                     //UIImageをReturnする
+        
     }
 
 
